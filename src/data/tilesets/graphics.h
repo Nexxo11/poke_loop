b51132cb278d5e2b49370985d2c77cca59db8aa4
Nexxo17 <gerez.angel1899@gmail.com>
2025-09-04 02:26:56 -0300

    triple_layer

--- src/data/tilesets/graphics.h
+++ src/data/tilesets/graphics.h
@@ -1634,3 +1634,22 @@ const u16 gTilesetPalettes_UnionRoom[][16] =
 };
 
 const u32 gTilesetTiles_UnionRoom[] = INCBIN_U32("data/tilesets/secondary/union_room/tiles.4bpp.fastSmol");
+
+const u16 gTilesetPalettes_Nature[][16] =
+{
+    INCBIN_U16("data/tilesets/primary/nature/palettes/00.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/01.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/02.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/03.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/04.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/05.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/06.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/07.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/08.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/09.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/10.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/11.gbapal"),
+    INCBIN_U16("data/tilesets/primary/nature/palettes/12.gbapal"),
+};
+
+const u32 gTilesetTiles_Nature[] = INCBIN_U32("data/tilesets/primary/nature/tiles.4bpp.lz");
--- src/data/tilesets/headers.h
+++ src/data/tilesets/headers.h
@@ -830,3 +830,14 @@ const struct Tileset gTileset_UnionRoom =
     .metatileAttributes = gMetatileAttributes_UnionRoom,
     .callback = NULL,
 };
+
+const struct Tileset gTileset_Nature =
+{
+    .isCompressed = TRUE,
+    .isSecondary = FALSE,
+    .tiles = gTilesetTiles_Nature,
+    .palettes = gTilesetPalettes_Nature,
+    .metatiles = gMetatiles_Nature,
+    .metatileAttributes = gMetatileAttributes_Nature,
+    .callback = NULL,
+};
